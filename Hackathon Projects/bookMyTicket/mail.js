936e472216029d853aa65a7b3b179b9f7aa9dabd
divyarajgurjar <163096296+divyarajgurjar@users.noreply.github.com>
2026-04-13 22:42:38 +0530

    signup, login and verify endpoints add

---
+++ Hackathon Projects/bookMyTicket/mail.js
@@ -0,0 +1,24 @@
+import nodemailer from 'nodemailer'
+import 'dotenv/config.js'
+const transporter = nodemailer.createTransport({
+  host: "sandbox.smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS,
+  },
+});
+
+const sendVerificationEmail = async (email, token) => {
+  const url = `http://localhost:8080/verify?token=${token}`;
+  
+  await transporter.sendMail({
+    from: '"Cinema Admin" <admin@cinema.com>',
+    to: email,
+    subject: "Verify Your Account",
+    html: `<p>Click <a href="${url}">here</a> to verify your account.</p>`,
+  });
+};
+
+// This is how you make it available to other files
+export { sendVerificationEmail };
